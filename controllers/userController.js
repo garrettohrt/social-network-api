dfa47e30a4edebf819f975c4bdb6d153fbb0563f
gmun <garrettohrt@gmail.com>
2023-01-16 20:20:01 -0600

    create user and get all users routes working

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,27 @@
-const { User,Thought } = require('../models');
+const { User } = require('../models');
 
 module.exports = {
     getAllUsers(req, res) {
         User.find({})
-        .select('-__v')
-        .sort({ _id: -1 })
-        .then()
-    }
-}
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(userData => res.json(userData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+    // getSingleUser(req, res) {
+
+    // }
+    createUser({ params, body }, res) {
+        console.log(params);
+        User.create(body)
+            .then(userData => {
+                console.log(userData);
+                res.json(userData);
+            })
+            .catch(err => res.json(err));
+    },
+};
